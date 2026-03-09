d1f0faa86b3f6e7abd8b50b4ef3a0222342b92ea
rlaisqls <rlaisqls@gmail.com>
2026-03-09 19:00:55 +0900

    solve 1509

---
+++ 1509/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+세준이는 어떤 문자열을 팰린드롬으로 분할하려고 한다. 예를 들어, ABACABA를 팰린드롬으로 분할하면, {A, B, A, C, A, B, A}, {A, BACAB, A}, {ABA, C, ABA}, {ABACABA}등이 있다.
+
+분할의 개수의 최솟값을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 문자열이 주어진다. 이 문자열은 알파벳 대문자로만 이루어져 있고, 최대 길이는 2,500이다.
+*/
+
+int dp[2501][2501];
+chat s[2501];
+int n;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin>>s;
+  
+
+  return 0;
+}
