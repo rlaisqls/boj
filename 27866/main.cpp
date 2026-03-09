2c940963bb6b6aa567140cf2393b91d35d503763
rlaisqls <rlaisqls@gmail.com>
2026-03-09 19:00:55 +0900

    solve 27866

---
+++ 27866/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  int i;
+
+  cin>>s>>i;
+  cout<<s[i-1];
+
+  return 0;
+}
