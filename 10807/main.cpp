5bc556c4e13bea1b380ef33aae1119de190be664
rlaisqls <rlaisqls@gmail.com>
2026-03-09 19:00:55 +0900

    solve 10807

---
+++ 10807/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    int arr[101];
+    cin>>n;
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    cin>>m;
+
+    int cnt=0;
+    for (int i=0;i<n;i++) {
+        if (arr[i]==m) {
+            cnt++;
+        }
+    }
+
+    cout<<cnt;
+    return 0;
+}
